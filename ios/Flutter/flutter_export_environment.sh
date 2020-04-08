e74b50e4f1b869ba50c9eb0d275d4bfaf6f8ef64
stharana <stharana2013@gmail.com>
2020-04-09 01:00:53 +0530

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=F:\Campus\Academic\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=F:\Campus\Projects\Flutter\codecanyon-22764499-ecommerce-ui-kit-using-flutter\treva_shop_flutter"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=F:\Campus\Academic\Flutter\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"

fc770eca9a5de97734825e6586ca1bc23ae8cb31
stharana <stharana2013@gmail.com>
2020-04-09 01:24:27 +0530

    git ignore changed

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=F:\Campus\Academic\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\Campus\Projects\Flutter\codecanyon-22764499-ecommerce-ui-kit-using-flutter\treva_shop_flutter"
+export "FLUTTER_APPLICATION_PATH=F:\Campus\Projects\PothApp\Bookstore-flutter"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
